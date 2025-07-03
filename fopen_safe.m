321e3b549e131afac85d3460be275307746f4a5d
bentrainer <117783189+bentrainer@users.noreply.github.com>
2025-07-03 17:34:44 -0500

    persist onCleanup return variable

--- fopen_safe.m
+++ fopen_safe.m
@@ -1,4 +1,4 @@
 function fp = fopen_safe(varargin)
     fp = fopen(varargin{:});
-    evalin("caller", sprintf("onCleanup(@() fclose(%d));", fp));
+    evalin("caller", sprintf("persist_onCleanup_return = onCleanup(@() fclose(%d));", fp));
 end
