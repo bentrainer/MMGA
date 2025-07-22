d78c7fd3c70a47a16a30ce71e305fd397bd6baa6
bentrainer <117783189+bentrainer@users.noreply.github.com>
2025-07-22 17:05:08 -0500

    persist with a unique variable name

--- fopen_safe.m
+++ fopen_safe.m
@@ -1,4 +1,6 @@
 function fp = fopen_safe(varargin)
     fp = fopen(varargin{:});
-    evalin("caller", sprintf("persist_onCleanup_return = onCleanup(@() fclose(%d));", fp));
+
+    [~, tmp_fn] = fileparts(tempname()); % put a variable of random name in the caller for onCleanup()
+    evalin("caller", sprintf("persist_fopen_safe_%s = onCleanup(@() fclose(%d));", tmp_fn, fp));
 end
